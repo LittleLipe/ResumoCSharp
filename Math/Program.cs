22b7432f145de9e0e4f722108216e39dc78035ad
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 18:45:20 -0300

    first

---
+++ Math/Program.cs
@@ -0,0 +1,16 @@
+﻿// O método pode ser usado para encontrar o maior valor de x e y :Math.Max(x,y)
+// O método pode ser usado para encontrar o menor valor de x e y :Math.Min(x,y)
+// O método retorna a raiz quadrada de x :Math.Sqrt(x)
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Max(5, 2));  
+     }
+  }
+}
