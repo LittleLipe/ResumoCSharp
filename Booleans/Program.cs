b269e2e5fb33c462df0f395a9004b79f068ab3a8
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 19:18:32 -0300

    first

---
+++ Booleans/Program.cs
@@ -0,0 +1,27 @@
+﻿//Muitas vezes, na programação, você precisará de um tipo de dados que só pode ter um de dois valores, como:
+//SIM NÃO
+//LIGADO DESLIGADO
+//VERDADEIRO FALSO
+
+//Para isso, o C# possui um booltipo de dados, que pode assumir os valores trueou false.
+
+// Um tipo booleano é declarado com a palavra- boolchave e só pode receber os valores trueou false:
+
+// Uma expressão booleana é uma expressão C# que retorna um valor booleano: Trueou False.
+
+//Você pode usar um operador de comparação, como o operador maior que ( >) para descobrir se uma expressão (ou uma variável) é verdadeira:
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = 9;
+      Console.WriteLine(x > y); // returns True, because 10 is higher than 9
+    }
+  }
+}
---
+++ While loop/Program.cs
@@ -0,0 +1,23 @@
+﻿//Os loops podem executar um bloco de código desde que uma condição especificada seja alcançada.
+
+//Os loops são úteis porque economizam tempo, reduzem erros e tornam o código mais legível.
+
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 0;
+      do 
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+      while (i < 5);
+    }
+  }
+}
