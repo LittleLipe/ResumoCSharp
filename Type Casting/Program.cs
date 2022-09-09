8889815ee52c215e85e8f7a48f5dff36182c20da
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 18:41:44 -0300

    first

---
+++ Type Casting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TypeCasting;
+
+class Program
+{
+    static void Main()
+    {
+        
+       int n = 10;
+       double nd = n;
+
+        Console.WriteLine(n);
+        Console.WriteLine(nd);
+
+
+        double mydouble = 9.78;
+        int myint = (int) mydouble; 
+
+        // trocar o double para inteiro e modificar o seu valor 
+       Console.WriteLine(mydouble);
+       Console.WriteLine(myint);
+
+        Console.WriteLine(Convert.ToString(myint));
+        Console.WriteLine(Convert.ToDouble(myint));
+      
+
+    }
+}
---
+++ User Input/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+namespace UserInput;
+
+class Program 
+{
+    static void Main()
+    {
+        string nome;
+        int idade;
+
+        Console.WriteLine("Diga seu sobre nome Bill: \n");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Qual a sua idade filho do Bill? \n");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sobrenome do Bill " + nome + " e a idade do filho do Bill: " + idade);
+
+    }
+}
