ddb9916dc05db2a6e964ad2d19df1dcd8b49a61a
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 18:36:29 -0300

    first

---
+++ Data Type/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace datatype;
+
+class Program
+{
+    static void Main()
+    {
+        
+        bool BoraBill = 7 > 6;
+        
+        // falso ou true
+        
+        Console.WriteLine(BoraBill);
+
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace variables;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, c; a = b = c = 22; Console.WriteLine(a + b + c); // soma dos números em variables
+    }
+}
