c24d37c9bb2ab337debaec58a378253289962ef5
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 18:29:17 -0300

    first

---
+++ Comments/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace comments;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bora Billl"); 
+        Console.WriteLine("Bora filho do Billl"); 
+
+        // 1 - Comentar
+        // 2 - Várias linhas
+
+        
+    }
+}
