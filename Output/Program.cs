5c32dc87d4d50a4588df7ced2f8daab5911ec759
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 18:27:01 -0300

    first

---
+++ Output/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Output;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Hello World!");
+        Console.Write("I am Learning C#");
+        Console.Write("It is awesome!");
+    }
+}
