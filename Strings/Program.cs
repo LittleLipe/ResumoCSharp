f5a9603868ef674556be8dc59857a8102dc05a2b
Little Lipe <felipe4080gta@hotmail.com>
2022-09-09 18:47:31 -0300

    first

---
+++ Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿// Uma stringvariável contém uma coleção de caracteres entre aspas duplas:
+// Crie uma variável do tipo stringe atribua um valor a ela: string greeting = "Hello";
+// Uma string em C# é na verdade um objeto, que contém propriedades e métodos que podem realizar certas operações em strings. Por exemplo, o comprimento de uma string pode ser encontrado com a Lengthpropriedade:
+// string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstName = "John ";
+      string lastName = "Doe";
+      string name = string.Concat(firstName, lastName);
+      Console.WriteLine(name);
+    }
+  }
+}
